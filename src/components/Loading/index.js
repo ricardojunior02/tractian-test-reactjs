bed18075f7dbb975e9f32456bf8b313854ab1edc
ricardojunior02 <ricardojunior199502@gmail.com>
2021-05-01 16:41:30 -0300

    Componente de loading do Dashboard

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import { Spin } from 'antd';
+
+const Loading = () => {
+  return (
+    <Container>
+      <Spin />
+    </Container>
+  )
+}
+
+export default Loading;
