c0f56c4a16838dd9b5a91a9027d3cd3b1e77c684
ricardojunior02 <ricardojunior199502@gmail.com>
2021-05-01 16:42:41 -0300

    Componente de retorno de aviso

---
+++ src/components/Warning/index.js
@@ -0,0 +1,13 @@
+
+import { Container } from './styles';
+
+const Warning = ({ message }) => {
+  return (
+    <Container>
+        <h2>{message}</h2>
+    </Container>
+  )
+}
+
+
+export default Warning;
